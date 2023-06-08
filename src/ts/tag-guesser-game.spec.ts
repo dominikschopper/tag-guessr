befe2fffc421d9e8437399f9a55cb8fe7a441536
dominik schopper <dominik.schopper@cofinpro.de>
2023-06-08 14:51:25 +0200

    feat(test) make jest work

---
+++ src/ts/tag-guesser-game.spec.ts
@@ -0,0 +1,10 @@
+import { TagGuessrGame, tagGuessrGame } from "./tag-guessr-game";
+
+describe('the TagGuessr Game Functionality', () => {
+    describe('starting and running the game', () => {
+        it('should return 0s as elapsed time when the game is not yet started', () => {});
+        it('should return the elapsed time, when the game is started', () => {});
+        it('should return the elapsed time, when the game is stopped', () => {});
+        it('should reset the time', () => {});
+    });
+});
